824e5cc353c1ce85e78e30b9359d06428982943d
zhen <770226915@qq.com>
2020-05-21 16:35:08 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === "production" ? "/vuetodolist" : "/",
-//   outputDir: "docs",
-// };
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/vuetodolist" : "/",
+  outputDir: "docs",
+};
